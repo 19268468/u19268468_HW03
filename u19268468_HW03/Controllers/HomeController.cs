6b76930eecfa4ab719d8b0aea757eb7c07754c2c
MoeSharp <MoeSharp@users.noreply.github.com>
2022-06-09 23:29:51 +0200

    Update 02

--- u19268468_HW03/Controllers/HomeController.cs
+++ u19268468_HW03/Controllers/HomeController.cs
@@ -11,13 +11,37 @@ namespace u19268468_HW03.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+           
 
             return View();
         }
+           
+        [HttpGet]
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            if (files != null && files.ContentLength > 0)
+            {
+                // extract only the filename
 
-        
+                var fileName = Path.GetFileName(files.FileName);
 
-       
+                // store the file inside ~/App_Data/uploads folder
+
+                var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+
+                // The chosen default path for saving
+
+                files.SaveAs(path);
+            }
+            // redirect back to the index action to show the form once again
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
